91f4c96896f34d3adfbcf6025a89b723390ccfe2
MickePros <Mikael_Pros@hotmail.com>
2024-09-30 09:45:33 +0200

    Add project files.

---
+++ Garage_1.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Garage_1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
